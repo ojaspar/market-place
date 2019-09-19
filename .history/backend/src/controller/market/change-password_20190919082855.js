e40a7fbda8a68bf2ad094d2236a3eded674c54e3
Divine Ogbonna <ojaspar@gmail.com>
2019-09-19 08:52:26 +0100

    reset password

--- .history/backend/src/controller/market/change-password_20190919082855.js
+++ .history/backend/src/controller/market/change-password_20190919082855.js
@@ -25,4 +25,3 @@ const changePassword = async (req, res, next) => {
     }
 
 }
-module.exports = changePassword;
---
+++ .history/backend/src/controller/market/reset_20190919085204.js
@@ -0,0 +1,22 @@
+const MarketModel = require('../../models/market/Market');
+const jwt = require('jsonwebtoken');
+const { secret } = require('../../../config/token');
+const createError = require('http-errors');
+const reset = async (req, res, next) => {
+    const { email } = req.body;
+    try {
+        // find email
+        const market = await MarketModel.findOne({ email });
+        if (!market) return next(createError(400, 'Market not found'));
+        if (!market.active) return next(createError(403, 'Account not activated'));
+        const payload = {
+            email: market.email,
+            expires: Date.now() + 30000
+        }
+        const token = jwt.sign(JSON.stringify(payload), secret);
+        res.send(200).send({ token })
+    } catch (e) {
+        return next(createError(500, e.message))
+    }
+}
+module.exports = reset;
